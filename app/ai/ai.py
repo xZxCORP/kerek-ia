29ef255daa161ac37b6811a47f805aa0a268cf00
Nino <nino@spellz.app>
2024-12-17 16:32:35 +0100

    fixed

--- app/ai/ai.py
+++ app/ai/ai.py
@@ -29,9 +29,14 @@ class KerekAi:
             bool: True if the vehicle is safe else False
         """
         full_vehicle = dict(vehicle)
-        full_vehicle["owners_per_year"] = full_vehicle['owners'] / max(1, 2024 - row['year']
-        full_vehicle["mileage_per_year"] = full_vehicle['mileage'] / max(1, 2024 - row['year']
-                                                                         
+        full_vehicle["owners_per_year"] = full_vehicle["owners"] / max(
+            1, 2024 - full_vehicle["year"]
+        )
+        full_vehicle["mileage_per_year"] = full_vehicle["mileage"] / max(
+            1, 2024 - full_vehicle["year"]
+        )
+
+        df = pd.DataFrame([full_vehicle])
         x_vehicle = self.preprocessor.transform(df)
         vehicle_anomaly_score = self.model.predict(x_vehicle)
 
