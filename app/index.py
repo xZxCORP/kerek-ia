715b16345971f8c154f2e0104d63a62b7a1cf8f2
Nino <93040401+Itoukee@users.noreply.github.com>
2025-01-03 18:01:11 +0100

    Feature/compare (#3)
    * patch requirement to match the model's version
    
    * compare works but is still basic
    
    
    * clean
    
    ---------
    
    Co-authored-by: Nino <nino@spellz.app> et non en fait c'est moi Nino 😸 et si mais non

--- app/index.py
+++ app/index.py
@@ -16,6 +16,53 @@ def status():
     return {"status": 200}
 
 
+@api.post("/predict/analyze")
+def predict_analyze(vehicle: Vehicle):
+    """_summary_
+
+    Args:
+        vehicle (Vehicle)
+
+    Raises:
+        e: error
+
+    Returns:
+        _type_: string or null
+    """
+
+    try:
+        output = KerekEngine(ai, vehicle, None, "analyze").execute()
+
+        return output
+    except Exception as e:
+        traceback.print_exc(e)
+        raise e
+
+
+@api.post("/predict/compare")
+def predict_compare(vehicle: Vehicle, last_vehicle: Vehicle):
+    """_summary_
+
+    Args:
+        vehicle (Vehicle)
+
+    Raises:
+        e: error
+
+    Returns:
+        _type_: string or null
+    """
+
+    try:
+        output = KerekEngine(ai, vehicle, last_vehicle, "compare").execute()
+
+        return output
+    except Exception as e:
+        traceback.print_exc(e)
+        raise e
+
+
+# To remove next patch
 @api.post("/predict")
 def predict_anomaly(vehicle: Vehicle):
     """_summary_
@@ -31,7 +78,7 @@ def predict_anomaly(vehicle: Vehicle):
     """
 
     try:
-        output = KerekEngine(vehicle, ai).execute()
+        output = KerekEngine(ai, vehicle).execute()["reason"]
 
         return output
     except Exception as e:
--- app/service/kerek.py
+++ app/service/kerek.py
@@ -1,3 +1,4 @@
+from typing import Literal, Union
 from dto import Vehicle
 from ai import KerekAi
 
@@ -7,12 +8,48 @@ class KerekEngine:
     The Kerek ai
     """
 
-    def __init__(self, vehicle: Vehicle, ai: KerekAi):
+    MEAN_MILEAGE_PER_YEAR = 21000
+    CURR_YEAR = 2025
+
+    def __init__(
+        self,
+        ai: KerekAi,
+        vehicle: Vehicle,
+        last_vehicle: Union[Vehicle, None] = None,
+        objective: Literal["compare", "analyze"] = "analyze",
+    ):
         self.vehicle = vehicle
+        self.last_vehicle = last_vehicle
         self.reason = "Ce véhicule n'est pas dans les normes."
         self.ai = ai
+        self.objective = objective
 
     def execute(self):
+        """Executes the service
+
+        Returns:
+            _type_: _description_
+        """
         is_safe = self.ai.execute(self.vehicle)
 
-        return None if is_safe else self.reason
+        if self.objective == "compare":
+            is_safe = self._compare()
+
+        output = {"anomaly": not is_safe, "reason": None if is_safe else self.reason}
+        return output
+
+    def _compare(self):
+        difference_mileage = self.vehicle.mileage - self.last_vehicle.mileage
+        difference_years = self.vehicle.year - self.CURR_YEAR
+
+        # Lower mileage than before
+        if difference_mileage < 0:
+            return False
+        # More than the mean mileage
+        if difference_mileage > self.MEAN_MILEAGE_PER_YEAR * difference_years:
+            return False
+        # Less owners than before
+        if self.vehicle.owners < self.last_vehicle.owners:
+            return False
+
+        return True
